3399dd8679a873d522b6bdf73754b07fea0be28b
Fblind <fblind@gmail.com>
2020-05-06 23:43:16 -0300

    add analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,10 @@ export default {
   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-165687640-1'
+    }]
   ],
   /*
   ** Nuxt.js modules
